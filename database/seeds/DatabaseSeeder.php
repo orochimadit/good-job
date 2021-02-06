ae14d049591cfce405adc02c829cdd19fa50886d
Aditya Pratama Badra <orochimadit@gmail.com>
2021-02-06 12:53:41 +0700

    seeder post job

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call([ 
             RoleTableSeeder::class, 
-            UserTableSeeder::class,]);
+            UserTableSeeder::class,
+            PostTableSeeder::class,]);
     }
 }
